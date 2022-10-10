4c4246372e6fce8d200d9758e022efb9c78b275d
Solomon <solgardner@icloud.com>
2022-10-10 17:26:56 -0600

    Changed this, that, and the other thing

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Yay"
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
